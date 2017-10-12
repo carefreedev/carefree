42c900671fbc43bd30ad1c99b3c7f478c8e30601
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-10-12 16:30:06 +0200

    Add initial carefree.compile module w/.program()
    --HG--
    branch : zimmermann

---
+++ carefree/compile.py
@@ -0,0 +1,28 @@
+from textwrap import dedent
+
+from path import Path
+
+import Boost.Cons
+import zcons
+
+import carefree
+
+
+def program(cppfile):
+    name = str(Path(cppfile).splitext()[0])
+    builddir = Path(cppfile).realpath().dirname()
+    SConstruct = builddir / cppfile + '.scons'
+    SConstruct.write_text(dedent("""
+    from path import Path
+
+    import cfo.scons
+
+
+    env = cfo.scons.Environment({!r})
+
+    env.Prepend(CPPPATH={!r})
+    env.Append(LIBS=['msvcrtd'])
+
+    env.Program({!r}, {!r})
+    """.format(name, carefree.INCLUDE, name, cppfile)))
+    zcons.scons(['-f', SConstruct], DEBUG=True)
